07dfede449bd7521d7b0fd655e93455e1caa896c
UzSoftic <softuzb@gmail.com>
2023-04-30 17:01:16 +0500

    update

--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/dashboard', DashboardController::class, 'index')->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
