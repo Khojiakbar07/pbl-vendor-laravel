fa63aa3ab1399bfb782ab62a74713f696fe423a3
UzSoftic <softuzb@gmail.com>
2023-05-01 17:06:59 +0500

    Revert "fixed buygs"
    This reverts commit b1b0d118db022f5ba944afbceb9dd072983d533a.

--- routes/admin.php
+++ routes/admin.php
@@ -5,19 +5,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-
-Route::middleware(['auth', 'verified'])->group(function () {
-
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-    Route::prefix('profile')->group(function (){
-        Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
+Route::get('/dashboard', DashboardController::class, 'index')->middleware(['auth', 'verified'])->name('dashboard');
 
 
-
-
-
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
