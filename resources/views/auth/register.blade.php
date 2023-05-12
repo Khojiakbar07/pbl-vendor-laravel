ab98bc170e93adccee05f309b8ce7f791c731536
uzsoftic <softuzb@gmail.com>
2023-05-12 23:45:29 +0500

    fixed test bugs

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,17 +1,12 @@
 @extends('layouts.admin_login')
 
 @section('content')
-    <!-- Login -->
+    <!-- Register Card -->
     <div class="card">
-        <div class="card-body">@extends('layouts.admin_login')
-
-@section('content')
-                <!-- Register Card -->
-                <div class="card">
-                    <div class="card-body">
-                        <!-- Logo -->
-                        <div class="app-brand justify-content-center mb-4 mt-2">
-                            <a href="{{ route('home') }}" class="app-brand-link gap-2">
+        <div class="card-body">
+            <!-- Logo -->
+            <div class="app-brand justify-content-center mb-4 mt-2">
+                <a href="{{ route('home') }}" class="app-brand-link gap-2">
                   <span class="app-brand-logo demo">
                     <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path
@@ -42,101 +37,104 @@
                       />
                     </svg>
                   </span>
-                                <span class="app-brand-text demo text-body fw-bold ms-1">{{ env('APP_NAME') }}</span>
-                            </a>
-                        </div>
-                        <!-- /Logo -->
-                        <h4 class="mb-1 pt-2">Adventure starts here 🚀</h4>
-                        <p class="mb-4">Make your app management easy and fun!</p>
+                    <span class="app-brand-text demo text-body fw-bold ms-1">{{ env('APP_NAME') }}</span>
+                </a>
+            </div>
+            <!-- /Logo -->
+            <h4 class="mb-1 pt-2">Adventure starts here 🚀</h4>
+            <p class="mb-4">Make your app management easy and fun!</p>
 
-                        <form id="formAuthentication" class="mb-3" method="POST" action="{{ route('register') }}">
-                            <div class="mb-3">
-                                <label for="name" class="form-label">{{ __('Name') }}</label>
-                                <input
-                                    type="text"
-                                    class="form-control"
-                                    id="name"
-                                    name="name"
-                                    placeholder="Enter your name"
-                                    value="{{ old('name') }}"
-                                    required autofocus autocomplete="name"
-                                />
-                            </div>
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Email</label>
-                                <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email" required autocomplete="email" />
-                            </div>
-                            <div class="mb-3 form-password-toggle">
-                                <label class="form-label" for="password">Password</label>
-                                <div class="input-group input-group-merge">
-                                    <input
-                                        type="password"
-                                        id="password"
-                                        class="form-control"
-                                        name="password"
-                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                                        aria-describedby="password"
-                                        role="presentation" autocomplete="off"
-                                        required
-                                    />
-                                    <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 form-password-toggle">
-                                <label class="form-label" for="password_confirmation">{{ __('Confirm Password') }}</label>
-                                <div class="input-group input-group-merge">
-                                    <input
-                                        type="password"
-                                        id="password_confirmation"
-                                        class="form-control"
-                                        name="password_confirmation"
-                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                                        aria-describedby="password"
-                                        role="presentation" autocomplete="off"
-                                        required
-                                    />
-                                    <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
-                                </div>
-                            </div>
-
-                            <div class="mb-3">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
-                                    <label class="form-check-label" for="terms-conditions">
-                                        I agree to
-                                        <a href="javascript:void(0);">privacy policy & terms</a>
-                                    </label>
-                                </div>
-                            </div>
-                            <button class="btn btn-primary d-grid w-100">{{ __('Sign up') }}</button>
-                        </form>
-
-                        <p class="text-center">
-                            <span>Already have an account?</span>
-                            <a href="{{ route('login') }}">
-                                <span>Sign in instead</span>
-                            </a>
-                        </p>
-
-                        <div class="divider my-4">
-                            <div class="divider-text">or</div>
-                        </div>
-
-                        <div class="d-flex justify-content-center">
-                            <a href="javascript:;" class="btn btn-icon btn-label-facebook me-3">
-                                <i class="tf-icons fa-brands fa-facebook-f fs-5"></i>
-                            </a>
-
-                            <a href="javascript:;" class="btn btn-icon btn-label-google-plus me-3">
-                                <i class="tf-icons fa-brands fa-google fs-5"></i>
-                            </a>
-
-                            <a href="javascript:;" class="btn btn-icon btn-label-twitter">
-                                <i class="tf-icons fa-brands fa-twitter fs-5"></i>
-                            </a>
-                        </div>
+            <form id="formAuthentication" class="mb-3" method="POST" action="{{ route('register') }}">
+                @csrf
+                <div class="mb-3">
+                    <label for="name" class="form-label">{{ __('Name') }}</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="name"
+                        name="name"
+                        placeholder="Enter your name"
+                        value="{{ old('name') }}"
+                        required autofocus autocomplete="name"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" class="form-control" id="email" name="email"
+                           placeholder="Enter your email" required autocomplete="email"/>
+                </div>
+                <div class="mb-3 form-password-toggle">
+                    <label class="form-label" for="password">Password</label>
+                    <div class="input-group input-group-merge">
+                        <input
+                            type="password"
+                            id="password"
+                            class="form-control"
+                            name="password"
+                            placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                            aria-describedby="password"
+                            role="presentation" autocomplete="off"
+                            required
+                        />
+                        <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
                     </div>
                 </div>
-                <!-- Register Card -->
+
+                <div class="mb-3 form-password-toggle">
+                    <label class="form-label"
+                           for="password_confirmation">{{ __('Confirm Password') }}</label>
+                    <div class="input-group input-group-merge">
+                        <input
+                            type="password"
+                            id="password_confirmation"
+                            class="form-control"
+                            name="password_confirmation"
+                            placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                            aria-describedby="password"
+                            role="presentation" autocomplete="off"
+                            required
+                        />
+                        <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms"/>
+                        <label class="form-check-label" for="terms-conditions">
+                            I agree to
+                            <a href="javascript:void(0);">privacy policy & terms</a>
+                        </label>
+                    </div>
+                </div>
+                <button class="btn btn-primary d-grid w-100">{{ __('Sign up') }}</button>
+            </form>
+
+            <p class="text-center">
+                <span>Already have an account?</span>
+                <a href="{{ route('login') }}">
+                    <span>Sign in instead</span>
+                </a>
+            </p>
+
+            <div class="divider my-4">
+                <div class="divider-text">or</div>
+            </div>
+
+            <div class="d-flex justify-content-center">
+                <a href="javascript:;" class="btn btn-icon btn-label-facebook me-3">
+                    <i class="tf-icons fa-brands fa-facebook-f fs-5"></i>
+                </a>
+
+                <a href="javascript:;" class="btn btn-icon btn-label-google-plus me-3">
+                    <i class="tf-icons fa-brands fa-google fs-5"></i>
+                </a>
+
+                <a href="javascript:;" class="btn btn-icon btn-label-twitter">
+                    <i class="tf-icons fa-brands fa-twitter fs-5"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- Register Card -->
 @endsection
