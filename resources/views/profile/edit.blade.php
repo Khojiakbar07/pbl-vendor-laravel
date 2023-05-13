e46daf6bf4e814d0038b964b742b2698a01fbf62
UzSoftic <softuzb@gmail.com>
2023-05-13 17:56:06 +0500

    fixed tests

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,30 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.admin')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+@section('content')
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mb-4">
+                <h5 class="card-header">{{ __('Edit') }}</h5>
+                <form method="POST" action="{{ route('profile.update', auth()->id()) }}">
+                    <input type="hidden" name="_method" value="PUT">
+                    @csrf
+                    <div class="card-body">
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+                        @include('layouts.admin.inc.form_error')
+
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">{{ __('Name') }}</label>
+                            <input type="text" class="form-control" placeholder="{{ __('Name') }}" name="name" value="{{ auth()->user()->name }}">
+                        </div>
+
+                        <div>
+                            <button type="submit" class="btn btn-lg btn-success my-3">{{ __('Submit') }}</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</x-app-layout>
+
+@endsection
