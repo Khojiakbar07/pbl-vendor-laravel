5f8f8c53c70940e44c0d96d3be1d0632bf360860
uzsoftic <softuzb@gmail.com>
2023-05-15 14:16:49 +0500

    some updates

--- resources/views/shop/customer/create.blade.php
+++ resources/views/shop/customer/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card mb-4">
-                <h5 class="card-header">{{__('Create Customer')}}</h5>
+                <h5 class="card-header">{{__('Create')}}</h5>
                 <form method="POST" action="{{ route('customer.store') }}" enctype="multipart/form-data">
                     @csrf
                     <div class="card-body">
--- resources/views/shop/product/edit.blade.php
+++ resources/views/shop/product/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card mb-4">
-                <h5 class="card-header">Edit Product</h5>
+                <h5 class="card-header">{{ __('Edit') }}</h5>
                 <form method="POST" action="{{ route('product.update', $product->id) }}">
                     <input type="hidden" name="_method" value="PUT">
                     @csrf
