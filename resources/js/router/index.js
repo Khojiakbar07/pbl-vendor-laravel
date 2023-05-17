b1e52543ce98950a45a7aba56e4d15ffd6f5ec0b
uzsoftic <softuzb@gmail.com>
2023-05-17 13:10:08 +0500

    removed unused files

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,22 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Registration from '../Viwes/Registration.vue'
-import MainViwe from '../Viwes/MainViwe.vue'
 import Terminal from "@/components/Terminal/Terminal.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/admin/order/terminal',
-      name: 'menu',
-      component: MainViwe
-    },
-    {
       path: '/admin/order/terminal/tailwind',
       name: 'terminal',
       component: Terminal
     }
-
   ]
 })
 
